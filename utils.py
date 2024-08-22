7f6aac2378720f0751e203157b4a08570fe43cb8
Umar Khan <ufkhan97@gmail.com>
2024-08-22 02:04:21 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,39 +4,29 @@ import numpy as np
 from datetime import datetime, timezone
 import psycopg2 as pg
 
-## Helper functions to run queries
-## These assume you have database credentials stored in your secrets.toml file
+## Helper function to run queries
+##  assumes you have database credentials stored in your secrets.toml file
 
-def run_query(query):
-    """Run query and return results"""
+def run_query(query, params=None, database="grants"):
+    """Run a parameterized query on the specified database and return results as a DataFrame."""
     try:
-        conn = pg.connect(host=st.secrets["database"]["host"], 
-                           port=st.secrets["database"]["port"], 
-                           dbname=st.secrets["database"]["dbname"], 
-                           user=st.secrets["database"]["user"], 
-                           password=st.secrets["database"]["password"])
+        conn = pg.connect(host=st.secrets[database]["host"], 
+                            port=st.secrets[database]["port"], 
+                            dbname=st.secrets[database]["dbname"], 
+                            user=st.secrets[database]["user"], 
+                            password=st.secrets[database]["password"])
         cur = conn.cursor()
-        cur.execute(query)
+        if params is None:
+            cur.execute(query)
+        else:
+            cur.execute(query, params)
         col_names = [desc[0] for desc in cur.description]
         results = pd.DataFrame(cur.fetchall(), columns=col_names)
     except pg.Error as e:
         st.warning(f"ERROR: Could not execute the query. {e}")
     finally:
+        cur.close()
         conn.close()
-    return results
-
-def run_query_with_params(query, params):
-    conn = pg.connect(host=st.secrets["database"]["host"], 
-                           port=st.secrets["database"]["port"], 
-                           dbname=st.secrets["database"]["dbname"], 
-                           user=st.secrets["database"]["user"], 
-                           password=st.secrets["database"]["password"])
-    cur = conn.cursor()
-    cur.execute(query, params)
-    col_names = [desc[0] for desc in cur.description]
-    results = pd.DataFrame(cur.fetchall(), columns=col_names)
-    cur.close()
-    conn.close()
     return results
 
 
